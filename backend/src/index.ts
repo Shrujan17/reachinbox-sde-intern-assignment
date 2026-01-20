1b88ec387037694d1ceae606529f02da90b272e3
Shrujan <shrujangowda46@gmail.com>
2026-01-20 18:26:24 +0530

    Fix Express import for CommonJS production runtime

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express";
-import cors from "cors";
+import * as express from "express";
+import * as cors from "cors";
 import session from "express-session";
 import passport from "./auth/google";
 
